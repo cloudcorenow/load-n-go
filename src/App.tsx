34db4dcbd19aa219afebbf6ddb47e5d630bea187
cloudcorenow <138939871+cloudcorenow@users.noreply.github.com>
2026-05-14 13:25:32 -0700

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import About from './components/About';
+import Flyer from './components/Flyer';
 import HowItWorks from './components/HowItWorks';
 import Services from './components/Services';
 import Gallery from './components/Gallery';
@@ -16,7 +17,8 @@ export default function App() {
       <main>
         <Hero />
         <About />
-<HowItWorks />
+        <Flyer />
+        <HowItWorks />
         <Services />
         <Gallery />
         <Location />
